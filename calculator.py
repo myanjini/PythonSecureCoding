b02783174128934ece5b02f6181626c9f3a83fed
Park Changryum <myanjini@gmail.com>
2021-08-13 16:03:24 +0900

    Create calculator.py
    최초 등록

---
+++ calculator.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+def run_code(string):
+    try:
+        result = eval(string, {})
+        print("{} = {}".format(string, result))
+    except Exception as e:
+        print(repr(e))
+
+if __name__ == "__main__":
+     run_code(sys.argv[1])

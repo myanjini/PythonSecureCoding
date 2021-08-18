82b2aff0aa6d2c2c5f59435bad92f449e802110b
Park Changryum <myanjini@gmail.com>
2021-08-18 21:50:24 +0900

    Create download.py
    최초 등록

---
+++ download.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import request, send_file, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        help = request.form['help'] 
+        path = '~/PythonSecureCoding/helps/' + help
+        return send_file(path, as_attachment=True)
+
+    helps = ['Help-A', 'Help-B', 'Help-C', 'Help-D']
+    template = '''
+        <form action="/" method="post">
+          <select name="help">
+          {% for h in helps %}
+            <option value="{{h}}" selected>{{h}}</option>"
+          {% endfor %}
+          </select>
+          <input type="submit" value="Help"/>
+        </form>
+    '''
+    return render_template_string(template, helps=helps)
+
+if __name__ == "__main__":
+    app.run(debug=True)
